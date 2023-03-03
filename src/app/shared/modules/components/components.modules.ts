b8f45af71f7f9cc593d11a9e3a48d0b515123018
giuliana <giuliana.cecchetto@syloper.com>
2023-03-03 13:49:15 -0300

    feat: shared components

---
+++ src/app/shared/modules/components/components.modules.ts
@@ -0,0 +1,17 @@
+// Angular Imports
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule } from '@angular/forms'
+// Shared Imports
+import { MaterialModule } from '../material/material.module'
+import { FooterComponent, NavbarComponent } from '../../components'
+
+// const components = [CustomSnackbarComponent]
+const components: any[] = [NavbarComponent, FooterComponent]
+
+@NgModule({
+	declarations: [...components],
+	imports: [CommonModule, ReactiveFormsModule, MaterialModule],
+	exports: [...components],
+})
+export class ComponentsModule {}
