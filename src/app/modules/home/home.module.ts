110f71ed455e4be5e7c8e9f49eeda951912ad0ec
giuliana <giuliana.cecchetto@syloper.com>
2023-03-16 21:23:03 -0300

    feat: bootstrap and hero styling

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,13 +1,12 @@
 // Angular imports
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { ReactiveFormsModule } from '@angular/forms'
 
 // Shared Imports
 import { SectionsModule } from './sections/sections.module'
 
 @NgModule({
 	declarations: [],
-	imports: [CommonModule, ReactiveFormsModule, SectionsModule],
+	imports: [CommonModule, SectionsModule],
 })
 export class HomeModule {}
