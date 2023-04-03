647be92e83f387806a57e8a7f3316984ddf48661
giuliana <giuliana.cecchetto@syloper.com>
2023-04-03 18:57:44 -0300

    fix: responsive

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common'
 import { HomeComponent } from './home.component'
 import { ContactComponent, ExperienceComponent, HeroComponent } from './sections'
 
-const sections : any[] = [HeroComponent, ExperienceComponent, ContactComponent]
+const sections: any[] = [HeroComponent, ExperienceComponent, ContactComponent]
 @NgModule({
 	declarations: [...sections, HomeComponent],
 	imports: [CommonModule],
