f323b4622c3a8ff850f5d85140ced88f92a9b8cd
giuliana <giuliana.cecchetto@syloper.com>
2023-04-03 16:22:42 -0300

    feat: sections

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -6,48 +6,6 @@ import { Component } from '@angular/core'
 	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-	skills: Skill[] = [
-		{
-			name: 'HTML',
-			img: '/assets/images/icons/html-5.png',
-		},
-		{
-			name: 'CSS',
-			img: '/assets/images/icons/css-3.png',
-		},
-		{
-			name: 'Sass',
-			img: '/assets/images/icons/sass.png',
-		},
-		{
-			name: 'Bootstrap',
-			img: '/assets/images/icons/bootstrap.png',
-		},
-		{
-			name: 'Javascript',
-			img: '/assets/images/icons/javascript.png',
-		},
-		{
-			name: 'Angular',
-			img: '/assets/images/icons/angular-js.png',
-		},
-		{
-			name: 'React',
-			img: '/assets/images/icons/react.png',
-		},
-		{
-			name: 'Ionic',
-			img: '/assets/images/icons/ionic.png',
-		},
-		{
-			name: 'GitHub',
-			img: '/assets/images/icons/github.png',
-		},
-	]
-
 	constructor() {}
 }
-interface Skill {
-	name: string
-	img: string
-}
+
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HomeComponent } from './home.component'
+import { ContactComponent, ExperienceComponent, HeroComponent } from './sections'
 
+const sections : any[] = [HeroComponent, ExperienceComponent, ContactComponent]
 @NgModule({
-	declarations: [HomeComponent],
+	declarations: [...sections, HomeComponent],
 	imports: [CommonModule],
 })
 export class HomeModule {}
