a62b3d184def530b7c7d87cb286196b5caa2dbca
Giuliana <giulicecchetto@gmail.com>
2023-07-13 17:09:02 -0300

    feat: :art: feat: sidebar display

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core'
 
 @Component({
 	selector: 'app-navbar',
@@ -6,11 +6,11 @@ import { Component, Output, EventEmitter } from '@angular/core';
 	styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-	@Output() sidebarToggled = new EventEmitter<void>();
+	@Output() sidebarToggled = new EventEmitter<void>()
 
-	constructor() { }
+	constructor() {}
 
 	toggleSidebar() {
-		this.sidebarToggled.emit();
+		this.sidebarToggled.emit()
 	}
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -6,18 +6,18 @@ import { Component } from '@angular/core'
 	styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-	isSidebarOpen = false;
-	constructor() { }
+	isSidebarOpen = false
+	constructor() {}
 
 	toggleSidebar() {
-		this.isSidebarOpen = !this.isSidebarOpen;
+		this.isSidebarOpen = !this.isSidebarOpen
 		if (this.isSidebarOpen) {
-			let sidebar = document.querySelector('.sidebar') as HTMLElement;
+			let sidebar = document.querySelector('.sidebar') as HTMLElement
 			if (sidebar) {
 				sidebar.style.display = 'block'
 			}
 		} else {
-			let sidebar = document.querySelector('.sidebar') as HTMLElement;
+			let sidebar = document.querySelector('.sidebar') as HTMLElement
 			if (sidebar) {
 				sidebar.style.display = 'none'
 			}
